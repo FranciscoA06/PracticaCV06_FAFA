2007dda973b9eef18fa25b2b8a04be2a99a2ea49
FR1NC6SFDEZ <120632080+FR1NC6SFDEZ@users.noreply.github.com>
2025-03-05 18:58:18 +0100

    Estructura del Programa de FAFA”

--- PracticaCV_FAFA/Program.cs
+++ PracticaCV_FAFA/Program.cs
@@ -2,9 +2,16 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+
+        // CONSTANTES
+
+        // VARIABLES
+
+        // ENTRADA
+
+        // PROCESO
+
+        // SALIDA
+
     }
 }
